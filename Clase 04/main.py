563de7db6d43c6872e832949977379e59f47db21
Vega948 <gabrielavegafernandez09@gmail.com>
2026-05-23 11:37:42 -0600

    Clase 04

---
+++ Clase 04/main.py
@@ -0,0 +1,10 @@
+junio = 2
+agosto = 1
+mes = "agosto" 
+if mes == "Junio":
+    print(f"{junio} personas cumplen años en junio")
+elif mes =="agosto":
+    if agosto == 1:
+        print(f"{agosto} persona cumple años en agosto")
+    else:
+        print(f"{agosto}personas cumplen años en agosto")
