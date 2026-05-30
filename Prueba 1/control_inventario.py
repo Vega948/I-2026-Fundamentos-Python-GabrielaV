7a980d9a645a3a7c7a2d2d9f78622bafebad5370
Vega948 <gabrielavegafernandez09@gmail.com>
2026-05-30 12:03:12 -0600

    Evaluación 1

---
+++ Prueba 1/control_inventario.py
@@ -0,0 +1,19 @@
+#Registro de productos
+print("Ingrese la información de los productos que desea comprar")
+("print(input("Nombre del producto: ")")
+precio = int(input("ingrese el precio del produto: "))
+cantidad = int(input("ingrese la cantidad de productos: "))
+
+#cantidad miníma de productos
+if cantidad >= 1 and precio >= 1 : 
+    print("La información es válida")
+else:
+    print("la informacion que registró es inválida")
+
+print(input("Su producto ha sido registrado. ¿Desea registrar otro?"[Si]))
+while si 
+
+#cálculo del total
+total = precio * cantidad 
+print("El total a pagar es:", total)
+
